370ebd6137c2e21749681d5c9de5b2705a3925dd
Luke Ibeachum <luke_ibeachum@hotmail.co.uk>
2021-11-02 06:48:11 +0000

    scraper can take property name, property type, rooms, bathrooms and doesn't crash on scraping a web page with wrong elements

--- airbnbScrape/airbnbScrape/spiders/scrapeBNB.py
+++ airbnbScrape/airbnbScrape/spiders/scrapeBNB.py
@@ -1,29 +1,80 @@
 import scrapy
 import selenium
+from time import sleep
 
 from scrapy.selector import Selector
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-#driver = webdriver.Chrome("/Users/Luke/PycharmProjects/airbnbScrape/chromedriver")
-#driver.get('https://www.airbnb.co.uk/rooms/33090114?guests=1&adults=1')
 
-#scrapy_selector = Selector(text = driver.page_source)
-#scrapy_selector.xpath('//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/ol')
+# driver = webdriver.Chrome("/Users/Luke/PycharmProjects/airbnbScrape/chromedriver")
+# driver.get('https://www.airbnb.co.uk/rooms/33090114?guests=1&adults=1')
+
+# scrapy_selector = Selector(text = driver.page_source)
+# scrapy_selector.xpath('//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/ol')
 
 class scrapeBNB(scrapy.Spider):
     name = "airbnb"
 
-    def start_requests(self):
-        urls = [
-            'https://www.airbnb.co.uk/rooms/33090114',
-            'https://www.airbnb.co.uk/rooms/50633275sc',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    # def start_requests(self):
+    start_urls = (
+        'https://www.airbnb.co.uk/rooms/33090114',
+        'https://www.airbnb.co.uk/rooms/50633275',
+        'https://www.airbnb.co.uk/rooms/33571268'
+
+    )
+
+    # for url in urls:
+    # yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f'quotes-{page}.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log(f'Saved file {filename}')
+
+        #locating chromedriver. path may need to be changed to be relative
+        driver = webdriver.Chrome("/Users/Luke/PycharmProjects/airbnbScrape/chromedriver")
+
+        #using the urls from start_urls
+        driver.get(response.url)
+
+        #using try except to catch sites that give 403 error or any layout errors
+        try:
+            property_name = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+                (By.XPATH, '//*[@id="site-content"]/div/div[1]/div[1]/div[1]/div/div/div/div/section/div[1]/span/h1')))
+            property_beds = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+                (By.XPATH, '//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/ol/li[2]/span[2]')))
+            property_bathrooms = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+                (By.XPATH, '//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/ol/li[4]/span[2]')))
+            property_type = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+                (By.XPATH, '//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/div/h2')))
+
+            property_type_noHosted = property_type[0].text.split(" hosted", 1)
+            property_type_noHosted = property_type_noHosted[0]
+
+
+
+
+
+            yield {
+                'property name': ([property_name[0].text]),
+                'property beds': ([property_beds[0].text]),
+                'property bathrooms': ([property_bathrooms[0].text]),
+                'property type': ([property_type_noHosted])
+            }
+
+
+        except:
+
+            #airbnb website doesn't return a proper 403 error code so making a check for the
+            #text box that's shown on the website with the error code
+            error_code = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located(
+                (By.XPATH, '/html/body/div[5]/div/div/div[1]/div/div/div/section/div[2]/div')))
+
+            ##print(f'THIS IS THE ERROR CODE, {error_code[0].text}')
+
+            if "403" in error_code[0].text:
+                print("Site isn't allowing access.")
+
+        print("RUNNING THIS!!!!!!!")
+
+        driver.quit()
--- main.py
+++ main.py
@@ -17,10 +17,20 @@ driver = webdriver.Chrome("/Users/Luke/PycharmProjects/airbnbScrape/chromedriver
 driver.get("https://www.airbnb.co.uk/rooms/33090114?source_impression_id=p3_1635750796_viHgI2h1D%2BAutZov")
 
 scrapy_selector = Selector(text = driver.page_source)
+time.sleep(0.8)
 title = scrapy_selector.xpath("//span[@class='_1n81at5']//h1[@class='_fecoyn4']/text()").extract()
+time.sleep(0.8)
+
 
 teams = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="site-content"]/div/div[1]/div[1]/div[1]/div/div/div/div/section/div[1]/span/h1')))
+property_beds = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/ol/li[2]/span[2]')))
+property_bathrooms = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, '//*[@id="site-content"]/div[1]/div/div/section/div/div/div/div[1]/ol/li[4]/span[2]')))
 print([i.text for i in teams])
+print([property_beds[0].text])
+print([property_bathrooms[0].text])
+
+print(property_bathrooms[0])
+
 
 
 print("hi")
